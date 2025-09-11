4d661b7bd17336d2df3173479076ada2ad9271ca
codewmarx <garado602@gmail.com>
2025-09-11 17:51:34 +0800

    Learned assigning variables and values with checking data type using typeof

--- js-basics/day1.js
+++ js-basics/day1.js
@@ -1,8 +1,12 @@
 
-    function knowMe() {
-        // statement 1
-        document.getElementById('state1').innerHTML = "Hi, I am Marko Garado, a self-taught programmer."
 
-        // statement 2
-        document.getElementById('state2').innerHTML = "This time around, I am still a beginneer learning and exploring different technologies."
-    }
+const carName = "Volvo";
+let fName = "Marko";
+let carPrice = 1000000000;
+let student = true;
+
+document.getElementById("car").innerHTML = 
+"Hello I am " + fName + " and my first ever car is " + carName + " with a US Dollar price of " + "$" + carPrice; 
+
+document.getElementById("dataType").textContent = (typeof student);
+
