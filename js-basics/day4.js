3d557e2564c05977a2f3f108d543bf6c6abbc9d5
codewmarx <garado602@gmail.com>
2025-09-13 17:07:49 +0800

    Learned a very simple average calculation

--- js-basics/day4.js
+++ js-basics/day4.js
@@ -11,12 +11,27 @@
     // }
 
     // function for fahrenheit - celcius
-    function toCelsius() {
-        let fahrenheit = document.getElementById("userInput").value;
+    // function toCelsius() {
+    //     let fahrenheit = document.getElementById("userInput").value;
 
-        let result = (5/9) * (fahrenheit - 32);
+    //     let result = (5/9) * (fahrenheit - 32);
 
-        document.getElementById("show").innerHTML = "F - C value: " + result.toFixed(2);
+    //     document.getElementById("show").innerHTML = "F - C value: " + result.toFixed(2);
+    // }
+    
 
+    // Function Problem
+    // 1. Create a function that takes a list of exam scores and returns the average score. Then, use that function to find the average of these scores: 90, 85, 78, 92, 88
+    function computeAverage () {
+        let value1 = Number(document.getElementById("avg1").value);
+        let value2 = Number(document.getElementById("avg2").value);
+        let value3 = Number(document.getElementById("avg3").value);
+        let value4 = Number(document.getElementById("avg4").value);
+        let value5 = Number(document.getElementById("avg5").value);
+
+        let average = (value1 + value2 + value3 + value4 + value5) / 5;
+
+        document.getElementById("showAvg").innerHTML = "Your total average is: " + average.toFixed(2);
     }
+    
     
