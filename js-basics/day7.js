3494a261e502021d646648cc980422674e303070
codewmarx <garado602@gmail.com>
2025-09-16 21:01:56 +0800

    Fixed some simple array length problems

--- js-basics/day7.js
+++ js-basics/day7.js
@@ -13,18 +13,18 @@
 
     let userInput = window.prompt("Enter a new car: ");
 
-    // combined predefined array with user input
-    let combinedArray = carLists.concat(userInput);
-    
-    // total length of predefined array and user input
-    let arrayLength = combinedArray.length;
-
-    // last list of the array element
-    let carLast = combinedArray[combinedArray.length - 1];
-    
-    if (carLists.includes(userInput)) {
+    if (userInput === null || userInput.trim() === "") {
+        alert("Please enter an input!");
+    } else if (carLists.includes(userInput)) {
         alert("Input already exists!");
     } else {
         carLists.push(userInput);
-        document.getElementById("solving").innerHTML = "Updated cars: " + combinedArray + "<br> The length of the elements is " + arrayLength + "<br> The last added car in the element is " + carLast;
     }
+    
+    // total length of predefined array and user input
+    let arrayLength = carLists.length;
+
+    // last list of the array element
+    let carLast = carLists[carLists.length - 1];
+    
+    document.getElementById("solving").innerHTML = "Updated cars: " + carLists + "<br> The length of the elements is " + arrayLength + "<br> The last added car in the element is " + carLast;
