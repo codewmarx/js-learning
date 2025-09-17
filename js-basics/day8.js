7191831f456e2ef0133cbbf389134c37c706db0b
codewmarx <garado602@gmail.com>
2025-09-18 00:53:58 +0800

    Learned few simple javascript date objects

--- js-basics/day8.js
+++ js-basics/day8.js
@@ -1,5 +1,14 @@
 
     // const dates
-    const d = new Date();
+    const d = new Date();  
 
-    document.getElementById("dates").textContent = d;
+    // date objects
+    const seconds = d.getSeconds();
+    const mins = d.getMinutes();
+    const hrs = d.getHours();
+    const day = d.getDate();
+    const month = d.getMonth() + 1;
+    const year = d.getFullYear();
+
+    document.getElementById("dates").innerHTML = "Seconds count: " + seconds + "<br> Minutes count: " + mins + "<br> Hours count: " + hrs + "<br> Current date: " + day + "<br>Current month: " + month + "<br> Current year: " + year;
+ 
