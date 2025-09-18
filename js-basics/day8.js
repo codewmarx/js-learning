711bf26d7bedf17fca193718a6c688cf6b11b796
codewmarx <garado602@gmail.com>
2025-09-18 23:57:08 +0800

    Simple js date objects specifier with number limits

--- js-basics/day8.js
+++ js-basics/day8.js
@@ -1,6 +1,7 @@
 
     // const dates
-    const d = new Date();  
+    // specifying dates with 6 numbers: year, month day, hour, minute, second
+    const d = new Date(2003, 11, 29, 10, 20, 30);
 
     // date objects
     const seconds = d.getSeconds();
@@ -10,5 +11,11 @@
     const month = d.getMonth() + 1;
     const year = d.getFullYear();
 
-    document.getElementById("dates").innerHTML = "Seconds count: " + seconds + "<br> Minutes count: " + mins + "<br> Hours count: " + hrs + "<br> Current date: " + day + "<br>Current month: " + month + "<br> Current year: " + year;
+    document.getElementById("dates").innerHTML = "Seconds count: " + seconds + "<br> Minutes count: " + mins + "<br> Hours count: " + hrs + "<br> Current date: " + day + "<br>Current month: " + month + "<br> The current year is: " + year;  
+
+
+    // js current date and time prob
+    // const today = new Dat();
+
+    // document.getElementById("dates").innerHTML = today;
  
