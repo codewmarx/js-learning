54912a4c6f5d9e56f772f79af16038a2c2b5ebed
codewmarx <garado602@gmail.com>
2025-09-20 14:15:47 +0800

    Javascript date string formats: Readable format, UTC, ISO

--- js-basics/day8.js
+++ js-basics/day8.js
@@ -1,7 +1,7 @@
 
     // const dates
     // specifying dates with 6 numbers: year, month day, hour, minute, second
-    const d = new Date(2003, 11, 29, 10, 20, 30);
+    const d = new Date();
 
     // date objects
     const seconds = d.getSeconds();
@@ -11,11 +11,10 @@
     const month = d.getMonth() + 1;
     const year = d.getFullYear();
 
+
     document.getElementById("dates").innerHTML = "Seconds count: " + seconds + "<br> Minutes count: " + mins + "<br> Hours count: " + hrs + "<br> Current date: " + day + "<br>Current month: " + month + "<br> The current year is: " + year;  
 
+    // js date string formats
+    document.getElementById("read-format").innerHTML = "Readable format: " + d.toDateString() + "<br> UTC format: " + d.toUTCString() + "<br> ISO foramt: " + d.toISOString();
 
-    // js current date and time prob
-    // const today = new Dat();
-
-    // document.getElementById("dates").innerHTML = today;
  
