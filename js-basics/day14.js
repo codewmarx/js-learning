93eacff97c4e9e7706bd7dc7459fde82e0fa238d
codewmarx <garado602@gmail.com>
2025-10-06 17:07:54 +0800

    Simple  asterisk tree using for loop in javascript

--- js-basics/day14.js
+++ js-basics/day14.js
@@ -1,12 +1,16 @@
 
 // js loops = Loops can execute a block of code a number of times.
 
-// for loop
-const students = ["Marko", "Mark", "Marky", "Marc"];
-let text = "";
+// simple saterisk tree using for loop
+let tree = ' ';
 
-for (let i = 0; i < students.length; i++) {
-    text += students[i] + "<br>";
+for (let i = 0; i < 10; i++) {
+    for (let j = 10; j > i; j--) {
+        tree += ' ';
+    }
+    for (let k = 0; k < 2 * i - 1; k++) {
+        tree += '*';
+    }
+    tree += "\n";
 }
-
-document.getElementById("loops").innerHTML = text;
+console.log(tree);
