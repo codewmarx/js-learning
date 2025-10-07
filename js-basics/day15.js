fa6aff3df0c7b2deceae1b94de757e34890f742b
codewmarx <garado602@gmail.com>
2025-10-07 23:21:46 +0800

    Countdown timer using while loop

--- js-basics/day15.js
+++ js-basics/day15.js
@@ -2,11 +2,14 @@
 // javascript while loop
 // The while loop loops through a block of code as long as a specified condition is true.
 
-let i = 0;
-let text = "";
+// Mini Project - Countdown Timer (While Loop)
+// Goal: Print numbers from 10 down to 1 using a while loop, then print "Blast off!".
+let timer = 10;
+let text = '';
 
-    while (i < 10) {
-        text += "The num is: " + i + '\n';
-        i++;
+    while (timer > 0) {
+        text += 'Bomb exploding in ' + timer + '\n';
+        timer--;
     }
+    text += "Blast off";
     console.log(text);
