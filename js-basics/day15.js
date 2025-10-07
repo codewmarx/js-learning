44aa0b6d524d8dea54a7f3774ea59317628f6a93
codewmarx <garado602@gmail.com>
2025-10-07 22:54:51 +0800

    Javascript While Loop

---
+++ js-basics/day15.js
@@ -0,0 +1,12 @@
+
+// javascript while loop
+// The while loop loops through a block of code as long as a specified condition is true.
+
+let i = 0;
+let text = "";
+
+    while (i < 10) {
+        text += "The num is: " + i + '\n';
+        i++;
+    }
+    console.log(text);
