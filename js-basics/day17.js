2361cb22a41a4d7f9fab746122c64acaa2e46f7d
codewmarx <garado602@gmail.com>
2025-10-12 00:11:44 +0800

    Javascript continue statement

--- js-basics/day17.js
+++ js-basics/day17.js
@@ -1,13 +1,13 @@
 
-// javascript break statement
-// The break statement terminates the execution of a loop or a switch statement.
+// javascript continue statement
+// The continue statement skips the current iteration in a loop.
 
 let text = '';
 
 // break in a for loop
 for (let i = 1; i < 10; i++) {
-    if (i === 3) { 
-        break;
+    if (i === 5) { //once i = 5, it skips it 
+        continue;
     }
     text += "The num is: " + i + '\n';
 }
