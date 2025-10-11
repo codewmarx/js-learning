146d10f6fabc64f64496ece097558aa853615d7d
codewmarx <garado602@gmail.com>
2025-10-11 23:46:43 +0800

    Javascript break statement in for loop

--- js-basics/day17.js
+++ js-basics/day17.js
@@ -1,17 +1,15 @@
 
-// comparing for and while loop
-// A while loop is much the same as a for loop, with statement 1 and statement 3 omitted.
+// javascript break statement
+// The break statement terminates the execution of a loop or a switch statement.
 
-// while loop = used to collect cars from the cars array
-const cars = ["Honda", "Rusi", "BMW", "Ferrari"];
-cars.push("Toyota");
-
-let  i = 0;
 let text = '';
 
-while (cars[i]) { //pretty much the same except that I omitted statement 1 and 2 
-    text += cars[i] + '\n';
-    i++
+// break in a for loop
+for (let i = 1; i < 10; i++) {
+    if (i === 3) { 
+        break;
+    }
+    text += "The num is: " + i + '\n';
 }
 
 console.log(text);
