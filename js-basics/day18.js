354b5d9bde6107b8b4299b1aa93e6199d26b2e73
codewmarx <garado602@gmail.com>
2025-10-15 23:59:53 +0800

    Basic js string methods

---
+++ js-basics/day18.js
@@ -0,0 +1,24 @@
+
+// js basic string methods
+
+let text = "Pneumonoultramicroscopicsilicovolcanoconiosis";
+
+// 1. legth property
+let lngth = text.length;
+console.log(lngth);
+
+// 2. charAt() string method
+let char = text.charAt(1);
+console.log(char);
+
+// 3. charCodeAt() string method
+let utfCode = text.charCodeAt(0);
+console.log(utfCode);
+
+// 4. codePointAt() string method
+let codePnt = text.codePointAt(1);
+console.log(codePnt);
+
+// 5. Property accesss []
+let proprty = text[2];
+console.log(proprty);
