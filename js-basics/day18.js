a88be5db7c6df3f08025b8409a4dc70dafa6395e
codewmarx <garado602@gmail.com>
2025-10-17 00:42:30 +0800

    Javascript string methods pt-3

--- js-basics/day18.js
+++ js-basics/day18.js
@@ -30,7 +30,31 @@ let fullName = fname.concat(" ", lname);
 console.log(fullName);
 
 // 7. extracting string parts
-// a. js string slice*()
-let food = "Adobobo";
+// a. js string slice()
+let food = "Gwapogi";
 let extract = food.slice(3, 7);
 console.log(extract);
+
+// b. js string substring() 
+let extracted = food.substring(0, 5);
+console.log(extracted);
+
+// 8. js string toUpperCase()
+let myName = "marko";
+let toUpper = myName.toUpperCase();
+console.log(toUpper);
+
+// 9. js string toLowerCase() 
+let me = "POGI";
+let toLower = me.toLowerCase();
+console.log(toLower);
+
+// 10. js strijng isWellFormed()
+let word = "Hello World";
+let checker = word.isWellFormed();
+console.log(checker);
+
+// 11. js string trim() 
+let greeting = "    Good Morning    ";
+let trem = greeting.trim(); //trim() method removes whitespaces
+console.log(greeting.length, trem.length);
